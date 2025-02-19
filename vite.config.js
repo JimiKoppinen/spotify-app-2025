5caacc1c3fdd339a47fcbca8df54fbdc1398759f
Jimi Koppinen <jimi.koppinen@gmail.com>
2025-02-19 13:56:44 +0200

    #5 add navigation fallback

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,36 @@
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+  // https://vite.dev/config/
+  // export default defineConfig({
+  //   plugins: [react()],
+  // })
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:7071',
-        changeOrigin: true,
-        secure: false,
+  // export default defineConfig({
+  //   plugins: [react()],
+  //   server: {
+  //     proxy: {
+  //       '/api': {
+  //         target: 'http://localhost:7071',
+  //         changeOrigin: true,
+  //         secure: false,
+  //       },
+  //     },
+  //   },
+  // });
+
+  export default defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:7071', // Ensure it's IPv4
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
-});
+  });
+  
